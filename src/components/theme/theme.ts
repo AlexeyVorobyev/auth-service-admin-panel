727175823536fd58fd3fe2a126dba79984e0124f
lexxv <mister.alex49@yandex.ru>
2024-06-05 00:58:03 +0300

    refactored containerisation of client applications

--- src/components/theme/theme.ts
+++ src/components/theme/theme.ts
@@ -29,15 +29,15 @@ declare module '@mui/material/Button' {
 export const theme = createTheme({
     palette: {
         primary: {
-            main: '#9c27b0',
-            light: '#af52bf',
-            dark: '#6d1b7b',
+            main: '#f49011',
+            light: '#ffb91d',
+            dark: '#eb7727',
             contrastText: '#fff',
         },
         secondary: {
-            main: '#3d5afe',
-            light: '#637bfe',
-            dark: '#2a3eb1',
+            main: '#2196f3',
+            light: '#4dabf5',
+            dark: '#1769aa',
             contrastText: '#fff',
         },
         neutral: {
--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -3,7 +3,13 @@ type TConfig = {
     entrypointServiceNginxAddress: string
 }
 
+declare global {
+    interface Window {
+        SERVER_ENV: any
+    }
+}
+
 export const GLOBAL_CONFIG: TConfig = {
-    apiAuthServiceAddress: import.meta.env.VITE_APP_API_AUTH_SERVICE_ADDRESS,
-    entrypointServiceNginxAddress: import.meta.env.VITE_APP_ENTRYPOINT_SERVICE_NGINX_ADDRESS,
+    apiAuthServiceAddress: window.SERVER_ENV?.VITE_APP_API_AUTH_SERVICE_ADDRESS || import.meta.env.VITE_APP_API_AUTH_SERVICE_ADDRESS,
+    entrypointServiceNginxAddress: window.SERVER_ENV?.VITE_APP_ENTRYPOINT_SERVICE_NGINX_ADDRESS || import.meta.env.VITE_APP_ENTRYPOINT_SERVICE_NGINX_ADDRESS,
 }
